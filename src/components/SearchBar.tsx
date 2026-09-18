5b383a2d0059e22c4633c696c438cda4f448d36b
Theanordhoy <thea.n.k@hotmail.com>
2026-09-18 09:31:25 +0200

    added search bar with functionality

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
+  return (
+    <div>
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        placeholder={placeholder}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
+      />
+    </div>
+  );
+}
